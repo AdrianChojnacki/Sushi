3e43e0f38c0466ef641c63f749451d42462b684e
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2019-03-19 18:08:37 +0100

    Added AJAX to navigation.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,41 @@
-var swiper = new Swiper('.swiper-container', {
-    spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
-    autoplay: true
+$(document).ready(function() {
+
+    //----------------------------------------------------------------------------------------------------------- AJAX -
+
+    $('.navigation a:nth-child(1)').click(function(e) {
+        e.preventDefault();
+        $('#main-panel').load('content/o_nas_content.html');
+    });
+
+    $('.navigation a:nth-child(2)').click(function(e) {
+        e.preventDefault();
+        $('#main-panel').load('content/menu_content.html');
+    });
+
+    $('.navigation a:nth-child(3)').click(function(e) {
+        e.preventDefault();
+        $('#main-panel').load('content/dowoz_content.html');
+    });
+
+    $('.navigation a:nth-child(4)').click(function(e) {
+        e.preventDefault();
+        $('#main-panel').load('content/franczyza_content.html');
+    });
+
+    $('.navigation a:nth-child(5)').click(function(e) {
+        e.preventDefault();
+        $('#main-panel').load('content/kontakt_content.html');
+    });
+
+    //--------------------------------------------------------------------------------------------------------- SWIPER -
+
+    var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        autoplay: true
+    });
+
 });
